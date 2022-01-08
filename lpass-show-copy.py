f2b651e3f337c799babb524407e1815ac3ac2e4c
Håvard Moen <post@haavard.name>
2022-01-08 11:19:50 +0100

    clear clipboard for password entries after 30 seconds, run through black

--- lpass-show-copy.py
+++ lpass-show-copy.py
@@ -31,6 +31,7 @@ gi.require_version("Gdk", "3.0")
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gdk, Gtk, GLib
 
+
 class ErrorWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Error getting lastpass data")
@@ -46,6 +47,21 @@ class ErrorWindow(Gtk.Window):
         button = Gtk.Button.new_with_label("Ok")
         button.connect("clicked", Gtk.main_quit)
         box.add(button)
+
+
+class ClearClipboardWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Lastpass clipboard clear")
+        self.set_default_size(0, 0)
+        self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        self.set_focus_on_map(False)
+
+    def clear_clipboard(self):
+        self.show_all()
+        self.clipboard.clear()
+        self.clipboard.set_text("", 0)
+        GLib.timeout_add(100, Gtk.main_quit)
+
 
 class LastPassWindow(Gtk.Window):
     def __init__(self, entry):
@@ -65,28 +81,28 @@ class LastPassWindow(Gtk.Window):
         box_entries.attach(id_frame, 1, 1, 2, 1)
         lpass_id = Gtk.Entry()
         id_frame.add(lpass_id)
-        lpass_id.props.text = entry['id']
+        lpass_id.props.text = entry["id"]
         lpass_id.props.editable = False
 
         url_frame = Gtk.Frame(label="URL")
         box_entries.attach(url_frame, 1, 2, 2, 1)
         url = Gtk.Entry()
         url_frame.add(url)
-        url.props.text = entry['url']
+        url.props.text = entry["url"]
         url.props.editable = False
 
         username_frame = Gtk.Frame(label="Username")
         box_entries.attach(username_frame, 1, 3, 1, 1)
         username = Gtk.Entry()
         username_frame.add(username)
-        username.props.text = entry['username']
+        username.props.text = entry["username"]
         username.props.editable = False
 
         password_frame = Gtk.Frame(label="Password")
         box_entries.attach(password_frame, 2, 3, 1, 1)
         password = Gtk.Entry()
         password_frame.add(password)
-        password.props.text = entry['password']
+        password.props.text = entry["password"]
         password.props.editable = False
         password.set_visibility(False)
         password.props.input_purpose = Gtk.InputPurpose.PASSWORD
@@ -96,7 +112,7 @@ class LastPassWindow(Gtk.Window):
         note = Gtk.TextView()
         note_frame.add(note)
         note.set_wrap_mode(wrap_mode=Gtk.WrapMode.NONE)
-        note.props.buffer.set_text(text=entry['note'])
+        note.props.buffer.set_text(text=entry["note"])
         note.props.editable = False
 
         button_box = Gtk.ButtonBox(spacing=6)
@@ -123,16 +139,32 @@ class LastPassWindow(Gtk.Window):
         copy_note_button.connect("clicked", self.copy, "note")
 
     def copy(self, callback, item):
+        def create_clear_password_window(data=None):
+            clear_window = ClearClipboardWindow()
+            clear_window.show_all()
+            GLib.timeout_add(100, clear_window.clear_clipboard)
+            return False
+
         self.clipboard.set_text(self.entry[item], -1)
         self.hide()
-        GLib.timeout_add(100, Gtk.main_quit)
+        if item == "password":
+            GLib.timeout_add(30000, create_clear_password_window)
+        else:
+            GLib.timeout_add(100, Gtk.main_quit)
+
 
 def main():
     if len(sys.argv) != 2:
         print(f"Usage {sys.argv[0]} lastpassid")
         sys.exit(1)
 
-    ret = subprocess.run(["lpass", "show", "--json", sys.argv[1]], encoding='utf-8', timeout=30, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    ret = subprocess.run(
+        ["lpass", "show", "--json", sys.argv[1]],
+        encoding="utf-8",
+        timeout=30,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
     if ret.returncode == 0:
         try:
             entry = json.loads(ret.stdout)
@@ -147,6 +179,7 @@ def main():
     window.show_all()
 
     Gtk.main()
+
 
 if __name__ == "__main__":
     main()
